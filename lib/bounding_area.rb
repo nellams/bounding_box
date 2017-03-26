e9ceff6e036bb76ba98d09443bf6e50f039ab7f2
Sarah <nellamsem@gmail.com>
2017-03-25 17:53:08 -0700

    Added bounding area code

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,2 +1,21 @@
+require_relative 'bounding_box'
+
 class BoundingArea
+  def initialize(bounding_area)
+    @bounding_area = bounding_area
+  end
+
+  def boxes_contain_point?(x, y)
+    if @bounding_area.empty?
+      false
+    else !@bounding_area.empty?
+      @bounding_area.any? do |box|
+        if box.contains_point?(x, y)
+          true
+        else !box.contains_point?(x, y)
+          false
+        end
+      end
+    end
+  end
 end
